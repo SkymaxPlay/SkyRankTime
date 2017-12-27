66b11ec519371b4a883b1ee29cf5ff84ad5e0da9
SkymaxPlay <skymaxplay@gmail.com>
2017-12-27 14:37:45 +0100

    preparation  to v0.3

--- src/com/gmail/skymaxplay/skyranktime/SkyRankTime.java
+++ src/com/gmail/skymaxplay/skyranktime/SkyRankTime.java
@@ -17,6 +17,7 @@ public class SkyRankTime extends JavaPlugin {
     private static SkyRankTime instance;
     private CheckRanksThread checkRanksThread;
 
+    public static final String VERSION = "0.3";
     public boolean PexHook;
     public boolean GMHook;
 
